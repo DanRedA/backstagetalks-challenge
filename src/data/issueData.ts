4c413d005ce43a19bc1d1f1f21f2eeef89c70bf3
DanRedA <dan.red.arteria@gmail.com>
2024-01-17 14:02:48 -0600

    first commit

---
+++ src/data/issueData.ts
@@ -0,0 +1,54 @@
+const issueData = [
+	{
+		alt: "issue #7",
+		img: "/backstage/backstagetalks_cover_issue_7.png",
+		color: "#FF608C",
+		number: "7",
+		available: true,
+	},
+	{
+		alt: "issue #6",
+		img: "/backstage/backstagetalks_cover_issue_6.png",
+		color: "#FFFFFF",
+		number: "6",
+		available: true,
+	},
+	{
+		alt: "issue #5",
+		img: "/backstage/backstagetalks_cover_issue_5.png",
+		color: "#00C1B5",
+		number: "5",
+		available: false,
+	},
+	{
+		alt: "issue #4",
+		img: "/backstage/backstagetalks_cover_issue_4.png",
+		color: "#FF6519",
+		number: "4",
+		available: true,
+	},
+	{
+		alt: "issue #3",
+		img: "/backstage/backstagetalks_cover_issue_3.png",
+		color: "#FFBE00",
+		number: "3",
+		available: false,
+	},
+	{
+		alt: "issue #2",
+		img: "/backstage/backstagetalks_cover2017.png",
+		color: "#1D3FBB",
+		number: "2",
+		available: false,
+	},
+	{
+		alt: "issue #1",
+		img: "/backstage/backstagetalks_cover2016_n.png",
+		color: "#E30512",
+		number: "1",
+		available: true,
+	},
+]
+
+export default issueData;
+
